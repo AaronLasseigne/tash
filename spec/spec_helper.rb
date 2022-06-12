7974a1a1e70e1e0447773e7836786c6c22b46f20
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2022-06-12 18:47:26 -0500

    avoid warnings about "to_not raise"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'tash'
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.on_potential_false_positives = :nothing
   end
 
   config.mock_with :rspec do |mocks|
